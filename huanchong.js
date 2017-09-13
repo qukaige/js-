7e0612778f372955bfe9507dee9db2143f7712a6
qukaige <18800022773@163.com>
2017-09-13 19:44:00 +0800

    js-2

--- huanchong.js
+++ huanchong.js
@@ -1,22 +1,21 @@
-// 缓冲运动
+﻿
 function move(obj, json, callback) {
     clearInterval(obj.timer)
     obj.timer = setInterval(function () {
         var flag = true
-        for (var attr in json) { // 遍历传递的json
+        for (var attr in json) { 
             var cur = 0
             if (attr === 'opacity') {
                 cur = parseFloat(getStyle(obj, attr)) * 100
             } else {
-                cur = parseInt(getStyle(obj, attr)) // 获取元素的属性带 px单位
+                cur = parseInt(getStyle(obj, attr)) 
             }
             var spt = ( json[attr] - cur ) / 10
-            // 缓冲运动必须取整
             spt = spt > 0 ? Math.ceil(spt) : Math.floor(spt)
             if (!(cur === json[attr])) {
                 flag = false
                 if (attr === 'opacity') {
-                    obj.style.opacity = (spt + cur) / 100  // 其他游览器
+                    obj.style.opacity = (spt + cur) / 100  
                     obj.style.filter = 'alpha(opacity:' + (spt + cur) + ')' // ie下
                 } else {
                     obj.style[attr] = (spt + cur) + 'px'
@@ -25,15 +24,15 @@ function move(obj, json, callback) {
         }
         if(flag){
             clearInterval(obj.timer)
-            if (callback) callback() // 回调函数
+            if (callback) callback() 
         }
     }, 30)
 }
-// 获取元素样式
+
 function getStyle(obj, property) {
-    if (obj.currentStyle) { // ie下获取样式方法
+    if (obj.currentStyle) { 
         return obj.currentStyle[property]
-    } else { // 其他游览器获取样式方法
+    } else { 
         return getComputedStyle(obj, '')[property]
     }
 }
